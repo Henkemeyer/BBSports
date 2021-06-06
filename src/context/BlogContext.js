384922a534b10c7963811bcb58a1e1ea9e8e12e1
Tyler Henkemeyer <henkemeyer92@gmail.com>
2021-06-06 00:32:15 -0600

    Created a new Expo app

---
+++ src/context/BlogContext.js
@@ -0,0 +1,9 @@
+import React, { Children } from 'react';
+
+const BlogContext = React.createContext();
+
+export const BlogProvider = ({ children }) => {
+    return <BlogContext.Provider>
+        {children}
+    </BlogContext.Provider>
+};
